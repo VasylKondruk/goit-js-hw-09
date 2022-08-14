2239bb2ce72cdeed90211e65735647efdc36dac1
Vasyl Kondruk <vkondruk16@gmail.com>
2022-08-14 16:24:41 +0300

    update task-02

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,3 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
 const body = document.querySelector('body');
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
@@ -9,12 +5,22 @@ const stopBtn = document.querySelector('[data-stop]');
 startBtn.addEventListener('click', onCkickStartBtn);
 stopBtn.addEventListener('click', onClickStopBtn);
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 function onCkickStartBtn() {
   timerId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled');
 }
 
 function onClickStopBtn() {
   clearInterval(timerId);
+  stopBtn.setAttribute('disabled', 'disabled');
+  startBtn.removeAttribute('disabled');
 }
